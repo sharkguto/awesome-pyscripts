1e2b2f3b6a469c7c21195951a913a6ce9774d731
Gustavo Freitas <freitasmgustavo@gmail.com>
2017-08-29 11:16:15 -0300

    Update whois_simple.py

--- whois_simple.py
+++ whois_simple.py
@@ -1,47 +1,25 @@
-import sys
 import socket
 import re
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("whois.iana.org", 43))
 
-#convert string to bytes, socket need bytes
-
-domain_check = "sbt.show" #sys.argv[1]
-
-s.send(( domain_check + "\r\n").encode())
-
-#declares a bytes
-response = b""
-while True:
-    data = s.recv(4096)
-    response += data
-    if not data:
-        break
-s.close()
-
-del(s)
-
-#convert bytes to string
-text_parse = response.decode()
-
-# parse refer whois server
-refer_whois = re.findall(r'^refer\:\s+(.*)',text_parse,re.MULTILINE)
-
-print(refer_whois)
-
-for i in refer_whois:
+def check_domain(domain, whoisserver="whois.iana.org"):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((i, 43))
-    s.send(( domain_check + "\r\n").encode())
-    #declares a bytes
+    s.connect((whoisserver, 43))
+
+    s.send((domain + "\r\n").encode())
     response = b""
     while True:
-        data = s.recv(1024)
+        data = s.recv(4096)
         response += data
         if not data:
             break
     s.close()
-    text_zum = response.decode()
-    print(text_zum)
+    del(s)
 
+    refer_whois = re.findall(r'^refer\:\s+(.*)',response.decode(),re.MULTILINE)
+    if refer_whois:
+        return check_domain(domain, refer_whois[0])
+    else:
+        return response.decode()
+
+print(check_domain("google.com"))
